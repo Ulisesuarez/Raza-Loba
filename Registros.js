c2ca4f646ed359fb204e71b9f75caff38521461f
Ulises Suarez <ulises203@msn.com>
2018-10-29 16:42:37 +0100

    testRegistros

--- Registros.js
+++ Registros.js
@@ -20,3 +20,15 @@ function mostrarRegistro(registro){
     }
 
 }
+
+function testRegistros(){
+    registro.eventos=[ 'trabajar', 'tocar un árbol', 'hamburguesa', 'correr', 'twitter']
+    registro.pulpo= false
+    mostrarRegistro(registro)
+    console.log('El tamaño del registro (' + registro.eventos.length + ') es igual a 5? ' + (registro.eventos.length===5).toString())
+    console.log('Se transformó en pulpo? '+ registro.pulpo)
+    console.log('El evento hamburguesa está en el registro? esperado true: ' + contieneEvento('hamburguesa',registro.eventos) )
+    console.log('El evento pizza está en el registro? esperado false: ' + contieneEvento('pizza',registro.eventos) )
+}
+
+testRegistros()
